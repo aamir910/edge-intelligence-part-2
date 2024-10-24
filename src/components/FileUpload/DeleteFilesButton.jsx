af80574c1f2a3cdebb9de9a73b635d04afcf96d6
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-10-24 16:49:21 +0500

    here added the confirmation there

--- src/components/FileUpload/DeleteFilesButton.jsx
+++ src/components/FileUpload/DeleteFilesButton.jsx
@@ -1,12 +1,27 @@
-
 import React from 'react';
-import { Button, message } from 'antd'; // Import the message component
+import { Button, message, Modal } from 'antd'; // Import Modal
+
+const { confirm } = Modal;
 
 const DeleteFilesButton = () => {
+  const showConfirm = () => {
+    confirm({
+      title: 'Are you sure you want to delete all files?',
+      content: 'This action cannot be undone.',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk: handleSubmit,
+      onCancel() {
+        console.log('Cancel deletion');
+      },
+    });
+  };
+
   const handleSubmit = async () => {
     try {
-        const response = await fetch('https://entertainmentbuz.com/EDGE_INTELLIGENCE/delete_directory.php', {
-            method: 'POST',
+      const response = await fetch('https://entertainmentbuz.com/EDGE_INTELLIGENCE/delete_directory.php', {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -17,8 +32,7 @@ const DeleteFilesButton = () => {
       if (result.success) {
         message.success('Directory deleted successfully!', 2); // Success message with 2 seconds duration
       } else {
-        message.success('Directory already deleted : ' + (result.message),
-         2); // Error message with dynamic error
+        message.warning('Directory already deleted: ' + result.message, 2); // Error message with dynamic error
       }
     } catch (error) {
       console.error('Error deleting directory:', error);
@@ -27,7 +41,7 @@ const DeleteFilesButton = () => {
   };
 
   return (
-    <Button type="primary" onClick={handleSubmit} style={{ marginTop: '20px', backgroundColor: 'red' }}>
+    <Button type="primary" onClick={showConfirm} style={{ marginTop: '20px', backgroundColor: 'red' }}>
       Delete All Files
     </Button>
   );
